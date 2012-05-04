d10cfbe74a9d8d9a508fa797600b6527e15d76d9
George Avramidis <gavramidis@skroutz.gr>
2012-05-04 15:31:13 +0300

    get(power, id) now fetches the detail html string

--- lib/ravenloft/login.rb
+++ lib/ravenloft/login.rb
@@ -1,53 +1,72 @@
 module Ravenloft
-	SEARCH_URL = URL + "CompendiumSearch.asmx"
-	LOGIN_URL = URL + "login.aspx?page=monster&id=339"
+  SEARCH_URL = URL + "CompendiumSearch.asmx"
+  LOGIN_URL = URL + "login.aspx?page=monster&id=339"
 
-	extend self 
-	def login!(username, password)
-		Manager.new.tap {|m|
-			m.login!(username, password)
-		}
-	end
-	class Manager
+  extend self 
+  def login!(username, password)
+    Manager.new.tap {|m|
+      m.login!(username, password)
+    }
+  end
+  class Manager
+
+    attr_reader :logged_in
+
+    def initialize
+      @cookies = []
+      @logged_in = false
+    end
 
 
+    def login!(username, password)
 
-		def initialize
-			@cookies = []
-		end
+      # get event validation and viewstate
+      html = Nokogiri::HTML(open(LOGIN_URL).read)
+
+      params = {
+        "email" => username,
+        "password" =>  password,
+        "InsiderSignin" => "Sign In",
+        "__EVENTVALIDATION" => event_validation(html),
+        "__VIEWSTATE" => viewstate(html)
+      }
+
+      url = URI.parse(LOGIN_URL)
+      resp, data = Net::HTTP.post_form(url, params)
+
+      c = resp.get_fields('set-cookie')
+      planet_cookie = c.find {|i| i =~ /iPlanet/}.split(';').first
+      if planet_cookie
+        @cookies << planet_cookie
+        @logged_in = true
+      end
+
+      @cookies
+    end
 
 
-		def viewstate(html)
-			html.at_css("input[name=__VIEWSTATE]")[:value]
-		end
+    # @return [StringIO] the D&D Insider Response
+    def get_response(type, id)
+      url = URL + "#{type}.aspx?id=#{id}"
+      open(url, "Cookie" => @cookies.join('; '))
+    end
 
-		def event_validation(html)
-			html.at_css("input[name=__EVENTVALIDATION]")[:value]
-		end
 
-		def login!(username, password)
+    # @return [String] with the details of the requested resource
+    def get(type, id)
+      resp = get_response(type, id)
+      Nokogiri::HTML(resp.read).at_css("#detail").inner_html.strip
+    end
 
-			# get event validation and viewstate
-			html = Nokogiri::HTML(open(LOGIN_URL).read)
+    private 
 
-			params = {
-				"email" => username,
-				"password" =>  password,
-				"InsiderSignin" => "Sign In",
-				"__EVENTVALIDATION" => event_validation(html),
-				"__VIEWSTATE" => viewstate(html)
-			}
+    def viewstate(html)
+      html.at_css("input[name=__VIEWSTATE]")[:value]
+    end
 
-			url = URI.parse(LOGIN_URL)
-			resp, data = Net::HTTP.post_form(url, params)
+    def event_validation(html)
+      html.at_css("input[name=__EVENTVALIDATION]")[:value]
+    end
 
-			c = resp.get_fields('set-cookie')
-			@cookies << c.find {|i| i =~ /iPlanet/}.split(';').first
-		end
-
-		def get(type, id)
-			url = URL + "#{type}.aspx?id=#{id}"
-			Nokogiri::HTML(open(url, "Cookie" => @cookies.join('; ')).read)
-		end
-	end
+  end
 end
