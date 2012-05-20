abe48ef639283bade84dd6c22a723b374915eebf
Κώστας Καραχάλιος <kostas.karachalios@me.com>
2012-05-20 18:47:18 +0300

    Tests that require dnd_insider.yml are pendified when the file is missing

--- spec/lib/ravenloft/manager_spec.rb
+++ spec/lib/ravenloft/manager_spec.rb
@@ -3,7 +3,7 @@ describe Ravenloft::Manager do
   subject {Ravenloft::Manager.instance}
 
   before {
-    subject.reset!  
+    subject.reset!
   }
 
   it "should be a singleton" do
@@ -12,16 +12,22 @@ describe Ravenloft::Manager do
     }.to raise_error
   end
 
-  it "should login" do
-    subject.login!
-    subject.logged_in.should be
-  end
+  context "with credentials" do
+    before(:each) do
+      pending "These tests require dnd_insider.yml" unless File.exist?("dnd_insider.yml")
+    end
 
-  context "with wrong credentials" do
-    it "should raise error" do
-      expect {
-        subject.login!(username: 'lala', password: 'lala')
-      }.to raise_error(Ravenloft::AuthenticationError)
+    it "should login" do
+      subject.login!
+      subject.logged_in.should be
+    end
+
+    context "with wrong credentials" do
+      it "should raise error" do
+        expect {
+          subject.login!(username: 'lala', password: 'lala')
+        }.to raise_error(Ravenloft::AuthenticationError)
+      end
     end
   end
 end
